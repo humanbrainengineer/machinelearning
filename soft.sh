4452817f183f2e414397a79640af855077a6d7cf
Tei shimei <apacheindex@yahoo.com>
2016-06-06 00:02:36 +0800

    Create soft.sh

---
+++ soft.sh
@@ -0,0 +1,10 @@
+apt-get install python-pygraphviz
+apt-get install python-nautilus
+apt-get install python-pytaglib
+apt-get install python-gi
+apt-get install python-gobject
+apt-get install python-pytaglib
+
+apt-get install libgraphviz-dev
+apt-get install pkg-config
+apt-get install -y graphviz libgraphviz-dev pkg-config python-pip
